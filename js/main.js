5841b6222396abcd158b4b052ef989b46cb98be0
David <idavidcolin@gmail.com>
2019-01-06 19:22:22 -0500

    Getting input values

--- js/main.js
+++ js/main.js
@@ -1,2 +1,7 @@
 const compose = (...functions) => data =>
   functions.reduceRight((value, func) => func(value), data)
+
+let description = $('#description')
+let calories = $('#calories')
+let carbs = $('#carbs')
+let protein = $('#protein')
