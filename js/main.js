0f29f0d2856bc7437ef2059bdc1015b1b42a58e1
David <idavidcolin@gmail.com>
2019-01-06 19:40:16 -0500

    Validate empty inputs

--- js/main.js
+++ js/main.js
@@ -5,3 +5,34 @@ let description = $('#description')
 let calories = $('#calories')
 let carbs = $('#carbs')
 let protein = $('#protein')
+
+description.keypress(() => {
+  description.removeClass('is-invalid')
+})
+
+calories.keypress(() => {
+  calories.removeClass('is-invalid')
+})
+
+carbs.keypress(() => {
+  carbs.removeClass('is-invalid')
+})
+
+protein.keypress(() => {
+  protein.removeClass('is-invalid')
+})
+
+const validateInputs = () => {
+
+  description.val() ? '' : description.addClass('is-invalid')
+  calories.val() ? '' : calories.addClass('is-invalid')
+  carbs.val() ? '' : carbs.addClass('is-invalid')
+  protein.val() ? '' : protein.addClass('is-invalid')
+
+  if(
+    description.val() &&
+    calories.val() &&
+    carbs.val() &&
+    protein.val()
+  ) console.log('Agregar item a lista')
+}
